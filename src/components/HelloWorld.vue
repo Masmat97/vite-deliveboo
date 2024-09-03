ff796eea022a482f4a6cb70920618a49bdc2c515
Fianda0 <fiandanesegiuseppe000@gmail.com>
2024-09-03 16:28:36 +0200

    add axios api restaurants

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,43 +1,31 @@
-<script setup>
-import { ref } from 'vue'
+<script>
+import axios from 'axios';
 
-defineProps({
-  msg: String,
-})
 
-const count = ref(0)
+export default {
+  name: 'hello',
+
+  data() {
+    return {
+      base_url: 'http://localhost:8000/',
+      restaurant: ''
+    }
+  },
+  mounted() {
+    const url = `${this.base_url}api/restaurants`
+    axios.get(url).then(response => {
+      console.log(response.data);
+      this.restaurant = response.data.restaurant
+    })
+  }
+}
+
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
 
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
+  <h1>Apphome</h1>
 
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Learn more about IDE Support for Vue in the
-    <a
-      href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
-      target="_blank"
-      >Vue Docs Scaling up Guide</a
-    >.
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
 
-<style scoped>
-.read-the-docs {
-  color: #888;
-}
-</style>
+<style></style>
