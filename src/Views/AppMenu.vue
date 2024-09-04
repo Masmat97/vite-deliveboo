c80dc5617177733883e841c7da82f9e5cb4f42c0
Fianda0 <fiandanesegiuseppe000@gmail.com>
2024-09-04 12:55:53 +0200

    add DishCard - AppMenu

--- src/Views/AppMenu.vue
+++ src/Views/AppMenu.vue
@@ -1,8 +1,12 @@
 <script>
 import axios from 'axios';
+import DishCard from '../components/DishCard.vue';
 
 export default {
     name: 'AppMenu',
+    components: {
+        DishCard
+    },
     data() {
         return {
             base_url: 'http://localhost:8000/',
@@ -12,19 +16,37 @@ export default {
     mounted() {
         const url = `${this.base_url}api/restaurants/${this.$route.params.id}`
         axios.get(url).then(response => {
-            console.log(response.data);
-            this.restaurant = response.data.restaurant
+            this.restaurant = response.data.restaurants
+            console.log(this.restaurant);
         })
     }
 }
 </script>
 
 <template>
-<h1>Menù</h1>
-  </template>
-  
+    <div v-if="this.restaurant">
+        <div class="container">
+            <div class="row">
+                <div class="col-8">
+                    <h1>{{ restaurant.name }}</h1>
+                </div>
+                <div class="col-8">
+                    <div v-if="restaurant.image.startsWith('http')">
+                        <img :src="restaurant.image" class="card-img-top" alt="">
+                    </div>
+                    <div v-else>
+                        <img :src="url + 'storage/' + restaurant.image" class="card-img-top" alt="">
+                    </div>
+                </div>
+                <div>
+                    <DishCard v-for="dish in this.restaurant.dishes" :dish="dish" />
+                </div>
+                <div v-for="type in this.restaurant.types">
+                    tipologia: {{ type.name }}
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
 
-  <style scoped>
-  /* Add your styles here */
-  </style>
-  
+<style scoped></style>
