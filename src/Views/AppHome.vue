ff34ae0b8fcfc13bc7f18daacf3c636e6eb69d71
Fianda0 <fiandanesegiuseppe000@gmail.com>
2024-09-05 11:47:04 +0200

    Add filter types, Change logo

--- src/Views/AppHome.vue
+++ src/Views/AppHome.vue
@@ -31,36 +31,6 @@ export default {
 
 <template>
   <div>
-    <header>
-      <img src="#" alt="DeliveBoo Logo" class="logo">
-      <input type="search" placeholder="Cerca ristoranti o cibo..." class="search-bar">
-      <nav>
-        <ul>
-          <li><router-link to="/">Home</router-link></li>
-          <li><router-link to="/explore">Esplora</router-link></li>
-          <li v-if="user"><router-link to="/orders">Ordini</router-link></li>
-          <li v-if="!user"><router-link to="/login">Login</router-link></li>
-          <li v-if="!user"><router-link to="/about">About</router-link></li>
-          <li v-if="!user"><router-link to="/contact">Contact</router-link></li>
-
-
-          <li><router-link to="/cart">Carrello</router-link></li>
-        </ul>
-      </nav>
-    </header>
-    <!-- <section class="featured-restaurants">
-        <h2>Ristoranti del Momento</h2>
-        <div class="restaurant-list">
-          <div class="restaurant-item" v-for="restaurant in this.restaurants.data" :key="restaurant.id">
-            <img :src="restaurant.image" :alt="restaurant.name">
-            <div>
-              <h3>{{ restaurant.name }}</h3>
-              <p>{{ restaurant.description }}</p>
-              <span class="rating">{{ restaurant.rating }}</span>
-            </div>
-          </div>
-        </div>
-      </section> -->
 
     <AppRestaurants />
 
--- src/components/AppRestaurants.vue
+++ src/components/AppRestaurants.vue
@@ -2,62 +2,80 @@
 import axios from 'axios';
 
 export default {
-name: 'AppRestaurants',
+    name: 'AppRestaurants',
 
-data() {
-    return {
-        base_url: 'http://localhost:8000/',
-        restaurants: '',
-        searchType: '',
-        filteredRestaurants: ''
-     }
-},
- mounted() {
+    data() {
+        return {
+            base_url: 'http://localhost:8000/',
+            restaurants: '',
+            types: [], // add more types as needed
+            selectedTypes: [],
+            filteredRestaurants: ''
+        }
+    },
+    mounted() {
+        this.getTypes();
         const url = `${this.base_url}api/restaurants`
         axios.get(url).then(response => {
-            console.log(response.data);
             this.restaurants = response.data.restaurants
             this.filteredRestaurants = this.restaurants.data
-            console.log(this.restaurants);
+            console.log('Ristororanti' + this.restaurants);
 
         })
     },
-methods: {
-    searchRestaurants() {
-        const url = `${this.base_url}api/types/restaurant/${this.searchType}`
-        axios.get(url).then(response => {
-            console.log(response.data);
-            this.filteredRestaurants = response.data.restaurants
-        })
-     }
+    methods: {
+        getTypes() {
+            axios.get(`${this.base_url}api/types`).then(response => {
+                this.types = response.data.types;
+                console.log(response.data.types);
+
+            })
+        },
+        searchRestaurants() {
+            const url = `${this.base_url}api/types/restaurant`
+            const params = {
+                types: this.selectedTypes.map(type => type.name).join(',')
+            }
+            axios.get(url, { params }).then(response => {
+                console.log(response.data); // Add this line
+                this.filteredRestaurants = response.data.restaurants
+            })
+        }
     }
 }
 </script>
 <template>
-    <div class="container">
+    <div class="container-fluid">
+
         <div class="row">
-            <div class="col-12">
-                <input type="text" v-model="searchType" placeholder="Cerca per tipo">
+
+            <div class="col-2">
+                <p>Filtra per tipologia:</p>
+                <div v-for="type in this.types" :key="type">
+                    <input type="checkbox" :id="type" :value="type" v-model="selectedTypes">
+                    <label :for="type">{{ type.name }}</label>
+                </div>
                 <button @click="searchRestaurants">Cerca</button>
             </div>
-        </div>
-        <div class="row">
-            <div v-for="restaurant in filteredRestaurants" class="col-3 mt-5">
-                <div class="card" style="width: 18rem;">
 
-                    <div v-if="restaurant.image.startsWith('http')">
-                        <img :src="restaurant.image" class="card-img-top" alt="">
+            <div class="col-10 d-flex flex-wrap">
+                <div v-for="restaurant in filteredRestaurants" class="col-3 mt-5">
+                    <div class="card" style="width: 18rem;">
+
+                        <div v-if="restaurant.image.startsWith('http')">
+                            <img :src="restaurant.image" class="card-img-top" alt="">
+                        </div>
+                        <div v-else>
+                            <img :src="url + 'storage/' + restaurant.image" class="card-img-top" alt="">
+                        </div>
+
+                        <div class="card-body">
+
+                            <h5 class="card-title">{{ restaurant.name }}</h5>
+                            <router-link :to="{ name: 'restaurant', params: { id: restaurant.id } }">View
+                                Menù</router-link>
+                        </div>
                     </div>
-                    <div v-else>
-                        <img :src="url + 'storage/' + restaurant.image" class="card-img-top" alt="">
-                    </div>
-
-                    <div class="card-body">
-
-                        <h5 class="card-title">{{ restaurant.name }}</h5>
-                        <router-link :to="{ name: 'restaurant', params: { id: restaurant.id } }">View Menù</router-link>
-                    </div>
-
                 </div>
             </div>
         </div>
