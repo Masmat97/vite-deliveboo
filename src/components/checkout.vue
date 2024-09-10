c2bda8d2095d678bdfd1046972ee967459fc4319
Antonino <drakon9927@libero.it>
2024-09-10 19:00:18 +0200

    checkout link pagamento laravel

--- src/components/checkout.vue
+++ src/components/checkout.vue
@@ -11,14 +11,14 @@
           <h5>{{ item.dish.name }}</h5>
           <p>Prezzo: {{ item.dish.price }} €</p>
           <p>Quantità: {{ item.quantity }}</p>
-          <button @click="decrementQuantity(item.id)">-</button>
-          <button @click="incrementQuantity(item.id)">+</button>
-          <button @click="removeFromCart(item.id)">Rimuovi</button>
+          <button @click="decrementQuantity(item.dish.id)">-</button>
+          <button @click="incrementQuantity(item.dish.id)">+</button>
+          <button @click="removeFromCart(item.dish.id)">Rimuovi</button>
         </div>
       </div>
       <div class="cart-summary">
         <p>Total: {{ cartTotal }} €</p>
-        <button class="btn btn-primary">Procedi al pagamento</button>
+        <button class="btn btn-primary" @click="proceedToPayment">Procedi al pagamento</button>
         <button class="btn btn-danger" @click="emptyCart">Svuota carrello</button>
       </div>
     </div>
@@ -29,7 +29,7 @@
 import { eventBus } from '@/eventBus';
 
 export default {
-  name: 'Cart',
+  name: 'Checkout',
   data() {
     return {
       cart: [],
@@ -44,22 +44,31 @@ export default {
     updateCart() {
       this.cart = JSON.parse(localStorage.getItem('cart')) || [];
     },
+    proceedToPayment() {
+      if (this.cart.length === 0) {
+        alert('Il carrello è vuoto! Aggiungi alcuni piatti prima di procedere al checkout.');
+        return;
+      }
+      window.location.href = 'http://127.0.0.1:8000/checkout'; // Naviga all'URL del checkout Laravel
+    },
     removeFromCart(id) {
-      const updatedCart = this.cart.filter(item => item.id !== id);
+      const updatedCart = this.cart.filter(item => item.dish.id !== id);
       localStorage.setItem('cart', JSON.stringify(updatedCart));
       this.updateCart();
       eventBus.emit('cart-updated'); // Notifica dell'aggiornamento
     },
     incrementQuantity(id) {
-      const item = this.cart.find(item => item.id === id);
-      item.quantity++;
-      localStorage.setItem('cart', JSON.stringify(this.cart));
-      this.updateCart();
-      eventBus.emit('cart-updated'); // Notifica dell'aggiornamento
+      const item = this.cart.find(item => item.dish.id === id);
+      if (item) {
+        item.quantity++;
+        localStorage.setItem('cart', JSON.stringify(this.cart));
+        this.updateCart();
+        eventBus.emit('cart-updated'); // Notifica dell'aggiornamento
+      }
     },
     decrementQuantity(id) {
-      const item = this.cart.find(item => item.id === id);
-      if (item.quantity > 1) {
+      const item = this.cart.find(item => item.dish.id === id);
+      if (item && item.quantity > 1) {
         item.quantity--;
         localStorage.setItem('cart', JSON.stringify(this.cart));
         this.updateCart();
