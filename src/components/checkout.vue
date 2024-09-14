78de7b1150aba9924fd1585fa75db672947759c3
ClaPascarella <claudiopascarella1996@gmail.com>
2024-09-14 15:14:51 +0200

    modificato visualizzazione checkout

--- src/components/checkout.vue
+++ src/components/checkout.vue
@@ -1,38 +1,53 @@
 <template>
-  <div class="cart-container">
-    <h1>Riepilogo Ordini</h1>
-    <p v-if="restaurant">Stai ordinando da: <h4>{{ restaurant.name }}</h4></p>
-    <div id="confirm" style="display: none;">
-  <p id="confirm-message"></p>
-  <button class="m-1" id="yes-button">Sì</button>
-  <button class="m-1" id="no-button">No</button>
-</div>
-<!-- alert -->
-<div id="info" style="display: none;">
-    <p id="info-message"></p>
-    <button class="m-1 btn btn-primary" id="okey-button">Chiudi</button>
-  </div>
-    <div v-if="cart.length === 0" class="empty-cart">
-      <p>Il carrello è vuoto</p>
-    </div>
-    <div v-else>
-      <div v-for="item in cart" :key="item.dish.id" class="cart-item">
-        <img :src="item.dish.image" class="cart-item-image" alt="Product image">
-        <div class="cart-item-details">
-          <h5>{{ item.dish.name }}</h5>
-          <p>Prezzo: {{ item.dish.price }} €</p>
-          <p>Quantità: {{ item.quantity }}</p>
-          <button type="button" class="btn btn-outline-primary" @click="decrementQuantity(item)">-</button>
-          <button type="button" class="btn btn-outline-primary" @click="incrementQuantity(item)">+</button>
-          <button type="button" class="btn btn-outline-danger" @click="removeItemFromCart(item)">Rimuovi</button>
+  <div class="container-fluid cart-container">
+    <div class="row">
+      <div class="col-xl-3 col-lg-4 col-md-5 border border-danger ">
+        <h1 class="">Riepilogo Ordini</h1>
+        <p v-if="restaurant">Stai ordinando da:
+        <h4>{{ restaurant.name }}</h4>
+        </p>
+        <div id="confirm" style="display: none;">
+          <p id="confirm-message"></p>
+          <button class="m-1" id="yes-button">Sì</button>
+          <button class="m-1" id="no-button">No</button>
+        </div>
+        <!-- alert -->
+        <div id="info" style="display: none;">
+          <p id="info-message"></p>
+          <button class="m-1 btn btn-primary" id="okey-button">Chiudi</button>
+        </div>
+        <div v-if="cart.length === 0" class="empty-cart">
+          <p>Il carrello è vuoto</p>
+        </div>
+        <div v-else>
+          <div v-for="item in cart" :key="item.dish.id" class="cart-item">
+            <img :src="item.dish.image" class="cart-item-image" alt="Product image">
+            <div class="cart-item-details">
+              <h5>{{ item.dish.name }}</h5>
+              <p>Prezzo: {{ item.dish.price }} €</p>
+              <p>Quantità: {{ item.quantity }}</p>
+              <button type="button" class="btn btn-outline-primary" @click="decrementQuantity(item)">-</button>
+              <button type="button" class="btn btn-outline-primary" @click="incrementQuantity(item)">+</button>
+              <button type="button" class="btn btn-outline-danger" @click="removeItemFromCart(item)">Rimuovi</button>
+            </div>
+          </div>
         </div>
       </div>
-      <div class="cart-summary">
-        <p>Total: {{ cartTotal }} €</p>
-        <button class="btn btn-primary" @click="proceedToPayment">Procedi al pagamento</button>
-        <button class="btn btn-danger" @click="emptyCart">Svuota carrello</button>
+    </div>
+
+
+
+
+    <div class="row">
+      <div class="col-xl-3 col-lg-4 col-md-6 border border-danger mt-2 ">
+        <div class="p-2">
+          <p>Total: {{ cartTotal }} €</p>
+          <button class="btn btn-primary m-0" @click="proceedToPayment">Procedi al pagamento</button>
+          <button class="btn btn-danger" @click="emptyCart">Svuota carrello</button>
+        </div>
       </div>
     </div>
+
   </div>
 </template>
 
@@ -83,7 +98,7 @@ export default {
       const yesButton = document.getElementById("yes-button");
       const noButton = document.getElementById("no-button");
 
-      yesButton.addEventListener("click", function() {
+      yesButton.addEventListener("click", function () {
         const index = this.cart.findIndex(cartItem => cartItem.dish.id === item.dish.id);
         if (index !== -1) {
           this.cart.splice(index, 1);
@@ -94,33 +109,33 @@ export default {
         confirmBox.style.display = "none";
       }.bind(this));
 
-      noButton.addEventListener("click", function() {
+      noButton.addEventListener("click", function () {
         confirmBox.style.display = "none";
       });
     },
     incrementQuantity(item) {
-  const cart = JSON.parse(localStorage.getItem('cart')) || [];
-  const existingItem = cart.find(cartItem => cartItem.dish.id === item.dish.id);
-  if (existingItem) {
-    if (existingItem.quantity >= 15) {
-      const message = "Quantità massima raggiunta. Non puoi aggiungere più di 15 piatti.";
-      const confirmBox = document.getElementById("info");
-      document.getElementById("info-message").innerText = message;
-      confirmBox.style.display = "block";
+      const cart = JSON.parse(localStorage.getItem('cart')) || [];
+      const existingItem = cart.find(cartItem => cartItem.dish.id === item.dish.id);
+      if (existingItem) {
+        if (existingItem.quantity >= 15) {
+          const message = "Quantità massima raggiunta. Non puoi aggiungere più di 15 piatti.";
+          const confirmBox = document.getElementById("info");
+          document.getElementById("info-message").innerText = message;
+          confirmBox.style.display = "block";
 
-      // Add an event listener to the close button
-      const closeButton = document.getElementById("okey-button");
-      closeButton.addEventListener("click", function() {
-        confirmBox.style.display = "none";
-      }.bind(this));
+          // Add an event listener to the close button
+          const closeButton = document.getElementById("okey-button");
+          closeButton.addEventListener("click", function () {
+            confirmBox.style.display = "none";
+          }.bind(this));
 
-      return;
-    }
-    existingItem.quantity++;
-    localStorage.setItem('cart', JSON.stringify(cart));
-    eventBus.emit('cart-updated');
-  }
-},
+          return;
+        }
+        existingItem.quantity++;
+        localStorage.setItem('cart', JSON.stringify(cart));
+        eventBus.emit('cart-updated');
+      }
+    },
     decrementQuantity(item) {
       const cart = JSON.parse(localStorage.getItem('cart')) || [];
       const existingItem = cart.find(cartItem => cartItem.dish.id === item.dish.id);
@@ -139,7 +154,7 @@ export default {
           const yesButton = document.getElementById("yes-button");
           const noButton = document.getElementById("no-button");
 
-          yesButton.addEventListener("click", function() {
+          yesButton.addEventListener("click", function () {
             const index = cart.indexOf(existingItem);
             cart.splice(index, 1);
             localStorage.setItem('cart', JSON.stringify(cart));
@@ -147,7 +162,7 @@ export default {
             confirmBox.style.display = "none";
           });
 
-          noButton.addEventListener("click", function() {
+          noButton.addEventListener("click", function () {
             existingItem.quantity = 1;
             confirmBox.style.display = "none";
           });
@@ -155,26 +170,26 @@ export default {
       }
     },
     emptyCart() {
-  const confirmBox = document.getElementById("confirm");
-  confirmBox.style.display = "block";
-  const message = `Sei sicuro di svuotare il carrello?`;
-  document.getElementById("confirm-message").innerText = message;
+      const confirmBox = document.getElementById("confirm");
+      confirmBox.style.display = "block";
+      const message = `Sei sicuro di svuotare il carrello?`;
+      document.getElementById("confirm-message").innerText = message;
 
-  // Add event listeners to the confirm box buttons
-  const yesButton = document.getElementById("yes-button");
-  const noButton = document.getElementById("no-button");
+      // Add event listeners to the confirm box buttons
+      const yesButton = document.getElementById("yes-button");
+      const noButton = document.getElementById("no-button");
 
-  yesButton.addEventListener("click", function() {
-    localStorage.removeItem('cart');
-    this.cart = [];
-    eventBus.emit('cart-updated'); // Notifica dell'aggiornamento
-    confirmBox.style.display = "none";
-  }.bind(this));
+      yesButton.addEventListener("click", function () {
+        localStorage.removeItem('cart');
+        this.cart = [];
+        eventBus.emit('cart-updated'); // Notifica dell'aggiornamento
+        confirmBox.style.display = "none";
+      }.bind(this));
 
-  noButton.addEventListener("click", function() {
-    confirmBox.style.display = "none";
-  });
-}
+      noButton.addEventListener("click", function () {
+        confirmBox.style.display = "none";
+      });
+    }
   },
   mounted() {
     this.updateCart();
@@ -218,9 +233,10 @@ export default {
   text-align: right;
 }
 
-button{
+button {
   margin-left: 1rem;
 }
+
 .cart-summary {
   margin-top: 20px;
   text-align: right;
